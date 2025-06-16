16c87ff75f9bf640f809b7bd8401a5da93d78856
Alya <faizrahmanovaalfia@yandex.ru>
2025-06-16 18:45:13 +0800

    Revert "Добавлена функция элементарных операций"
    This reverts commit d1f71587989b21d4bf589b669fabcc72bcc0585a.

--- калькулятор/калькулятор.cpp
+++ калькулятор/калькулятор.cpp
@@ -8,32 +8,93 @@
 using namespace std; 
 
 
-double applyOp(char op, double b, double a, bool& error) {
-    switch (op) {
-    case '+': return a + b;
-    case '-': return a - b;
-    case '*': return a * b;
-    case '/':
-        if (b == 0) {
-            cout << "Ошибка: деление на ноль\n";
-            error = true;
-            return 0;
+bool isDigit(char ch) {
+    if (ch >= '0' && ch <= '9') {
+        return true;
+    }
+    return false;
+
+}
+
+bool simplIdentif(const string& currentString, int& position, bool& isError, vector<string>& identificator) {
+    //проверка - строка не закончилась и проверяеый символ - буква
+    if (position < currentString.size() && isDigit(currentString[position])) {
+        identificator.push_back(string(1, currentString[position]));
+        position++;
+        return true;
+    }
+    //обработка ошибок
+    isError = true;
+    position++;
+    return false;
+}
+
+bool operatorSign(const string& currentString, int& position, bool& isError)
+{
+    //проверка - строка не закончилась и проверяеый символ - оператор
+    if (position < currentString.size() && (currentString[position] == '+' || currentString[position] == '-' || currentString[position] == '*' || currentString[position] == '/' || currentString[position] == '#' || currentString[position] == ' '))
+    {
+        position++;
+        return true;
+    }
+    //обработка ошибок
+    isError = true;
+    position++;
+    return false;
+}
+
+bool simplExpression(const string& currentString, int& position, bool& isError, vector<string>& identificator, vector<string>& expression) {
+    //проверка на пустую строку
+    if (currentString.empty()) {
+        cout << "Ошибка: у вас пустая строка" << endl;
+        isError = true;
+        return false;
+    }
+
+    bool result = simplIdentif(currentString, position, isError, identificator);
+    bool flag = true;
+    string currentExpression;
+
+    while (position < currentString.size()) {
+        //проверка ожидания и корректности оператора
+        if (flag) {
+            result &= operatorSign(currentString, position, isError);
+            flag = false;
         }
-        return a / b;
-    case '^': return pow(a, b);
-    case '#':
-        if (b < 0) {
-            cout << "Ошибка: корень из отрицательного числа\n";
-            error = true;
-            return 0;
+        else {
+            //проверка ожидания и корректности простого идентификатора
+            result &= simplIdentif(currentString, position, isError, identificator);
+            flag = true;
+
+            //формирование простых выражений
+            if (identificator.size() >= 2) {
+                currentExpression = identificator[identificator.size() - 2] + currentString[position - 2] + identificator[identificator.size() - 1];
+                expression.push_back(currentExpression);
+            }
         }
-        return sqrt(b);
-    default:
-        cout << "Ошибка: неизвестный оператор\n";
-        error = true;
-        return 0;
+    }
+    return result;
+}
+
+bool Analis(const string& currentString, int& position, bool& isError, vector<string>& identificator, vector<string>& expression) {
+    return simplExpression(currentString, position, isError, identificator, expression);
+}
+
+void runAnalis() {
+    int position = 0;
+    bool isError = true;
+    string currentString;
+    vector<string> identificator;
+    vector<string> expression;
+
+    cout << "Введите выражение для проверки" << endl;
+    cin.ignore();
+    getline(cin, currentString);
+    if (Analis(currentString, position, isError, identificator, expression) && position == currentString.size()) {
+        
     }
 }
+
 
 
 int main()
